29a3ae86c7d0ba8c6229ce163cefe9b1b7c72ae0
Anchalmantwal <93024283+Anchalmantwal@users.noreply.github.com>
2024-09-11 17:44:25 +0530

    Update IBookService.ts

--- src/interfaces/IBookService.ts
+++ src/interfaces/IBookService.ts
@@ -1,7 +1,7 @@
 import { Book } from "../entities/Book";
 export interface IBookService {
   getAllBooks(): Promise<Book[]>;
-  createBook(book: Book): Promise<Book>;
-  updateBook(bookId: number, book: Partial<Book>): Promise<Book>;
+  createBook(book: Book): Promise<Book | null>;
+  updateBook(bookId: number, book: Partial<Book>): Promise<Book | null>;
   deleteBook(bookId: number): Promise<void>;
 }
