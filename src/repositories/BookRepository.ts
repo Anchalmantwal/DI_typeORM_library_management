613be96bebe25c31d574142c47bec2e07b8459fa
Anchalmantwal <93024283+Anchalmantwal@users.noreply.github.com>
2024-09-11 17:44:54 +0530

    Update BookRepository.ts

--- src/repositories/BookRepository.ts
+++ src/repositories/BookRepository.ts
@@ -3,6 +3,8 @@ import { Repository } from "typeorm";
 import { Book } from "../entities/Book";
 import { IBookRepository } from "../interfaces/IBookRepository";
 import { AppDataSource } from "../config/dataSource";
+import { InternalServerError } from "../errors/InternalServerError";
+
 
 @injectable()
 export class BookRepository implements IBookRepository {
@@ -13,16 +15,50 @@ export class BookRepository implements IBookRepository {
   }
 
   public async findAll(): Promise<Book[]> {
-    return await this.repository.find();
+    try {
+      if(!Book){
+        throw new InternalServerError("Cannot get all Books");
+      }
+      return await this.repository.find();
+
+    } catch (err) {
+      console.log("The error is ",err);
+      return [];
+    }
   }
   public async findOne(id: number): Promise<Book | null> {
-    return this.repository.findOneBy({ id });
+    try {
+      const book = this.repository.findOneBy({ id });
+      if (book === null) {
+        throw new InternalServerError(`Cannot get the Book with ${id}`);
+      }
+      return book;
+    } catch (err) {
+      console.error(`Error finding book with id: ${id}`, err);
+      return null;
+    }
   }
-  public async save(book: Book): Promise<Book> {
-    return await this.repository.save(book);
+  public async save(book: Book): Promise<Book | null> {
+    try {
+      const _book = await this.repository.save(book);
+      if (_book === null) {
+        throw new InternalServerError("Cannot create the book");
+      }
+      return _book;
+    } catch (err) {
+      console.error(`Error in saving Book`, err);
+      return null;
+    }
   }
 
   public async remove(book: Book): Promise<void> {
-    await this.repository.remove(book);
+    try {
+      await this.repository.remove(book);
+      if(!book){
+        throw new InternalServerError("Cannot delete the book");
+      }
+    } catch (err) {
+      console.error(`Error in deleting the book`, err);
+    }
   }
 }
