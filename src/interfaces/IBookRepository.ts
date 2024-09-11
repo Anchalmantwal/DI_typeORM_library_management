3478ccb51f6f00b9e63252393e524553d33c2e25
Anchalmantwal <93024283+Anchalmantwal@users.noreply.github.com>
2024-09-11 17:44:06 +0530

    Update IBookRepository.ts

--- src/interfaces/IBookRepository.ts
+++ src/interfaces/IBookRepository.ts
@@ -2,6 +2,6 @@ import { Book } from "../entities/Book";
 export interface IBookRepository {
   findAll(): Promise<Book[]>;
   findOne(bookId: number): Promise<Book | null>;
-  save(book: Book): Promise<Book>;
-  remove(book: Book): Promise<void>;
+  save(book: Book): Promise<Book | null>;
+  remove(book: Book): Promise<void >;
 }
