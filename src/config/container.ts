a0eb0e2cb39f9722c8e467cc2082aa779287ccbc
Anchalmantwal <93024283+Anchalmantwal@users.noreply.github.com>
2024-09-11 16:26:11 +0530

    Update container.ts

--- src/config/container.ts
+++ src/config/container.ts
@@ -4,12 +4,10 @@ import { IBookRepository } from "../interfaces/IBookRepository";
 import { IBookService } from "../interfaces/IBookService";
 import { BookService } from "../services/BookService";
 import { BookRepository } from "../repositories/BookRepository";
-import BookController from "../controllers/BookController";
 
 const myContainer = new Container();
 
 myContainer.bind<IBookService>(TYPES.IBookService).to(BookService);
 myContainer.bind<IBookRepository>(TYPES.IBookRepository).to(BookRepository);
-myContainer.bind<BookController>(TYPES.BookController).to(BookController);
 
 export { myContainer };
